2bcdcacb52b50659f1a8eed4e1330b7d51110e76
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-16 00:22:50 +0500

    secondcharoq

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,7 +1,6 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -16,21 +15,15 @@ public class FileReader {
     }
 
     private Profile mapStringToProfile(String s) {
-        System.out.println(s);
-        String name = s.substring(("Name: ").length(),s.indexOf("Age: "));
-        String age = s.substring(s.indexOf("Age: ") + "Age: ".length(), s.indexOf("Email: "));
-        String email = s.substring(s.indexOf("Email") + "Email: ".length(), s.indexOf("Phone: "));
-        String phone = s.substring(s.indexOf("Phone") + "Phone: ".length());
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(email);
-        System.out.println(phone);
-        return new Profile(name,Integer.parseInt(age),email,Long.parseLong(phone));
+        return new Profile(s.substring(("Name: ").length(), s.indexOf("Age: "))
+                , Integer.parseInt(s.substring(s.indexOf("Age: ") + "Age: ".length(), s.indexOf("Email: "))),
+                s.substring(s.indexOf("Email") + "Email: ".length(), s.indexOf("Phone: ")),
+                Long.parseLong(s.substring(s.indexOf("Phone") + "Phone: ".length())));
     }
 
     private String mapFileToString(File file) {
         StringBuilder content = new StringBuilder();
-        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");) {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
             FileChannel channel = randomAccessFile.getChannel();
             ByteBuffer buffer = ByteBuffer.allocate((int) file.length());
             int read = channel.read(buffer);
